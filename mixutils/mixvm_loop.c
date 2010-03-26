0332594c0251f5b66e775757e2851f30f09c8cc0
Jose Antonio Ortega Ruiz <jao@gnu.org>
2010-03-26 18:21:11 +0100

    Fix for #28086 (compilation without Guile).

--- mixutils/mixvm_loop.c
+++ mixutils/mixvm_loop.c
@@ -1,7 +1,8 @@
 /* -*-c-*- -------------- mixvm_loop.c :
  * Implementation of mix vm command loop.
  * ------------------------------------------------------------------
- * Copyright (C) 2000, 2001, 2002, 2004, 2006, 2007, 2009 Free Software Foundation, Inc.
+ * Copyright (C) 2000, 2001, 2002, 2004, 2006, 2007, 2009, 2010 Free
+ * Software Foundation, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
  *
  */
 
@@ -134,7 +136,7 @@ mix_vmloop (int argc, char *argv[], gboolean initfile,
   mixguile_init (argc, argv, initfile, loop_, dis);
 #else
   (void) init_mixvm_ (file, use_emacs);
-  loop_ (argc, argv);
+  loop_ (NULL, argc, argv);
 #endif
 }
 
